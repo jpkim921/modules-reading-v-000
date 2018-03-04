f179a07c838663758b00c38a5332f0aebd7f6a12
Jayon Kim <jpkim921@gmail.com>
2018-03-04 19:40:36 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,23 +1,21 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-# class Dancer
-#   # To lend our two classes (Kid/Dancer) all of the methods of the Dance module, we use the include keyword like below
-#   # The include keyword allows our classes to use all of the methods of the included module as instance methods.
-#   include Dance
-#
-#
-#
-#
-#   attr_accessor :name
-#
-#   def initialize(name)
-#     @name = name
-#   end
-#
-# end
+class Dancer
+  # To lend our two classes (Kid/Dancer) all of the methods of the Dance module, we use the include keyword like below
+  # The include keyword allows our classes to use all of the methods of the included module as instance methods.
+  include Dance
+  extend MetaDancing
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
 
 
 class Dancer
-  extend MetaDancing
+
 end
