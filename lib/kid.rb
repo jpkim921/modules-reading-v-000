ce279190632b3bc1e1cf6dcc9065be5e3526b80c
Jayon Kim <jpkim921@gmail.com>
2018-03-04 19:31:00 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,4 +5,10 @@ class Kid
     # To lend our two classes (Kid/Dancer) all of the methods of the Dance module, we use the include keyword like below
     # The include keyword allows our classes to use all of the methods of the included module as instance methods.
     include Dance
+
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
 end
