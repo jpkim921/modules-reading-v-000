cf01553e6a19ef3f763ce33734590f791e79067d
Jayon Kim <jpkim921@gmail.com>
2018-03-04 19:49:27 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,10 +5,9 @@ class Dancer
   # # To lend our two classes (Kid/Dancer) all of the methods of the Dance module, we use the include keyword
   # # The include keyword allows our classes to use all of the methods of the included module as instance methods.
   # include Dance
-  # 
+  #
   # # In order to lend a module's methods to a class as class methods, we use the extend keyword
   # extend MetaDancing
-
 
 
   extend FancyDance::ClassMethods
