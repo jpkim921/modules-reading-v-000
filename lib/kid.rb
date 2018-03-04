31a1f456d348f39c611fa6207d1ab37e2bfb7383
Jayon Kim <jpkim921@gmail.com>
2018-03-04 19:48:53 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,11 +2,14 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  # To lend our two classes (Kid/Dancer) all of the methods of the Dance module, we use the include keyword like below
+  # To lend our two classes (Kid/Dancer) all of the methods of the Dance module, we use the include keyword
   # The include keyword allows our classes to use all of the methods of the included module as instance methods.
   include Dance
+
+  # In order to lend a module's methods to a class as class methods, we use the extend keyword
   extend MetaDancing
 
+  
   attr_accessor :name
 
   def initialize(name)
